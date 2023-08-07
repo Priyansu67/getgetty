d8d24e1c60ef0608049fc4781f37559221fa5c75
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-08-08 05:11:59 +0530

    New UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import GCLogo from "@/components/gclogo";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 max-h-screen">
       <h1 className="animate-text pb-3 font-bold text-center text-4xl sm:text-7xl bg-clip-text text-transparent bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 tracking-wider">
         G<span className="text-2xl sm:text-5xl">ET</span>G
         <span className="text-2xl sm:text-5xl">ETTY</span>
@@ -17,11 +17,11 @@ export default function Home() {
             "_blank"
           )
         }
-        className="absolute flex items-center w-40 h-12 rounded-lg pl-2 top-5 left-5 cursor-pointer hover:scale-105 border border-gray-300 border-opacity-50 hover:border-opacity-100"
+        className="absolute flex items-center w-fit pr-2 sm:pr-0 sm:w-40 h-12 rounded-lg pl-2 top-5 left-5 cursor-pointer hover:scale-105 border border-gray-300 border-opacity-50 hover:border-opacity-100"
       >
         <div className="flex flex-row items-center justify-center">
           <GCLogo className="w-[35px] h-full" />
-          <div className="flex flex-col items-start justify-center h-full pl-2 leading-3">
+          <div className="hidden sm:flex flex-col items-start justify-center h-full pl-2 leading-3">
             <p className="text-white text-[0.5rem]">Available in the</p>
 
             <p className="text-white text-[0.7rem]">Chrome Web Store</p>
